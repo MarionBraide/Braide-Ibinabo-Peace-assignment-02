98c3bc7b3ab856652d4f042eb916c98ac33f8e6c
Multistan <82952143+pyeonghwassi@users.noreply.github.com>
2022-07-13 23:32:48 +0000

    i did not know how to use replit well so i might have mistakenly deleted stuff sigh. also made 5 atempts i think

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+let arrayOfNumbers = [10, 20, 30, 40, 50, 60]
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sumTotal = 0
+  for(let i = 0; i < arrayOfNumbers.length; i ++){
+    sumTotal += arrayOfNumbers[i]
+  }
+  return sumTotal
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+sumOfNumbers(arrayOfNumbers);
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +35,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+for(let i = 0; i < arrayOfNumbers.length; i++){
+  if (arrayOfNumbers[i] % 2 == 0)
+    count++;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+    return count;
+}
+countEvenNumbers(arrayOfNumbers);
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +62,10 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let outcome = arrayOfNumbers.map(e => Math.trunc(e * 1.8 + 32))
+    return outcome;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
